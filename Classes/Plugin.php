0a5fcb19b007713569faa8e10d6a819fea1f3ecc
John Einselen <iaian7@iaian7.com>
2022-09-03 16:57:51 -0400

    Adding support for per-page image directories
    Original dev comment: "hacky but works"

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -2,12 +2,13 @@
 /**
  * Plugin class
  */
+
 namespace Phile\Plugin\An7\InlineMedia;
 use Phile\Exception;
 
 /**
   * InlineMedia
-  * version 0.3 modified 2016.08.04
+  * version 0.4 modified 2016.08.11
   *
   * Based on James Doyle's PhileInlineImage plugin and helped by the work of Dan Reeves and Philipp Schmitt
   * Recognises most image, Vimeo, and Youtube links, replacing them with embed code
@@ -23,37 +24,49 @@ use Phile\Exception;
 class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
 
 	public function __construct() {
+//		\Phile\Event::registerEvent('after_resolve_page', $this);
 		\Phile\Event::registerEvent('after_parse_content', $this);
         \Phile\Event::registerEvent('after_read_file_meta', $this);
 	}
 
 	public function on($eventKey, $data = null) {
+		if ($this->settings['page_dir'] == true) {
+			$dir = preg_replace("/[^\\/]+$/u", "", "/content".$_SERVER['REQUEST_URI']);
+			if (!is_dir(ROOT_DIR.$dir)) { // check if the folder exists
+				throw new Exception("The specified page directory does not exist or is not a directory.");
+			} else {
+				// hack together a file path, because I cannot figure out any other way...all file or path variables are protected?!
+				$path = preg_replace("/[^\\/]+$/u", "", \Phile\Utility::getBaseUrl()."/content".$_SERVER['REQUEST_URI']);
+				// The following only works within an "after_resolve_page" event, which of course limits it to just that event
+				// $path = preg_replace("/[^\\/]+$/u", "", \Phile\Utility::getBaseUrl()."/content".$data['pageId']);
+			}
+		} else {
+			if (!is_dir(ROOT_DIR.$this->settings['images_dir'])) { // check if the folder exists
+				throw new Exception("The path ".$this->settings['images_dir']." in the InlineMedia config does not exist or is not a directory.");
+			} else {
+				$path = ROOT_DIR.$this->settings['images_dir'];
+			}
+		}
+
 		if ($eventKey == 'after_read_file_meta') {
 			if (isset($data['meta']['media'])) {
-//				$data['meta']['media_embed'] = $this->filter_media($data['meta']['media']);
 				// completely silly, but because the filter expects paragraph tags when processing page content I'm adding them here so it's recognised correctly
-				$data['meta']['media_embed'] = $this->filter_media('<p>'.$data['meta']['media'].'</p>');
+				$data['meta']['media_embed'] = $this->filter_media('<p>'.$data['meta']['media'].'</p>', $path);
 			} else {
 				$data['meta']['media_embed'] = "";
 			}
 		} elseif ($eventKey == 'after_parse_content') {
-			// check and see if the folder exists
-			if (!is_dir(ROOT_DIR . $this->settings['images_dir'])) {
-				throw new Exception("The path ".$this->settings['images_dir']." in the PhileInlineMedia config does not exists or is not a directory.");
-			}
-			$content = $this->filter_media($data['content']);
+			$content = $this->filter_media($data['content'], $path);
 			$data['content'] = $content;
 		}
 	}
 
-	private function filter_media($content) {
+	private function filter_media($content, $path) {
 		// return nothing if no content is available for processing
 		if (!isset($content)) return null;
 		// body copy processing happens after markdown processing which means that the potential media content is wrapped in <p> tags
 
 		// Images
-		// find the path for images
-		$path = \Phile\Utility::getBaseUrl() . '/' . $this->settings['images_dir'];
 		$regex = "/(<p>)(.*?)\.(jpg|jpeg|png|gif|webp|svg)+(<\/p>)/i";
 		$replace = "\n".'<'.$this->settings['wrap_element'].' class="'.$this->settings['wrap_class_img'].'" style="background-image: url(\''.$path.'$2.$3\');"></'.$this->settings['wrap_element'].'>';
 		// replace image strings with image embeds
