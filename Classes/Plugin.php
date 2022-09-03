957879711717657ffa90686fc6240d8fcd486e1d
John Einselen <iaian7@iaian7.com>
2022-09-03 16:58:44 -0400

    Testing events

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -8,7 +8,7 @@ use Phile\Exception;
 
 /**
   * InlineMedia
-  * version 0.4 modified 2016.08.11
+  * version 0.5 modified 2016.08.11
   *
   * Based on James Doyle's PhileInlineImage plugin and helped by the work of Dan Reeves and Philipp Schmitt
   * Recognises most image, Vimeo, and Youtube links, replacing them with embed code
@@ -23,13 +23,30 @@ use Phile\Exception;
 
 class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
 
+	private $page_url = 'not set yet';
+
 	public function __construct() {
-//		\Phile\Event::registerEvent('after_resolve_page', $this);
+		\Phile\Event::registerEvent('after_resolve_page', $this); // Only event that gives access to pageId, but appears to only occur just before the two parse content events!
+
+//		\Phile\Event::registerEvent('before_render_template', $this);
+//		\Phile\Event::registerEvent('template_engine_registered', $this);
+//		\Phile\Event::registerEvent('after_render_template', $this);
+
+		\Phile\Event::registerEvent('before_read_file_meta', $this);
+		\Phile\Event::registerEvent('after_read_file_meta', $this);
+
+		\Phile\Event::registerEvent('before_load_content', $this); // Includes filePath but basically nothing else
+			\Phile\Event::registerEvent('after_load_content', $this); // Includes filePath, rawData, page, but meta and content are protected?
+
+		\Phile\Event::registerEvent('before_parse_content', $this);
 		\Phile\Event::registerEvent('after_parse_content', $this);
-        \Phile\Event::registerEvent('after_read_file_meta', $this);
 	}
 
+/*
 	public function on($eventKey, $data = null) {
+		print("\n\n\n\tEVENT: ".$eventKey."\n\n\n");
+		print_r(get_defined_vars());
+
 		if ($this->settings['page_dir'] == true) {
 			$dir = preg_replace("/[^\\/]+$/u", "", "/content".$_SERVER['REQUEST_URI']);
 			if (!is_dir(ROOT_DIR.$dir)) { // check if the folder exists
@@ -60,7 +77,66 @@ class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\Even
 			$data['content'] = $content;
 		}
 	}
+//*/
 
+
+	public function on($eventKey, $data = null) {
+		print("\n\n\tEVENT: ".$eventKey);
+		print("\n\t PageURL: ".$this->page_url);
+//		print_r(get_defined_vars());
+		if ($eventKey == 'after_resolve_page') {
+//			print_r(get_defined_vars());
+			print("\n\t PageID: ".$data['pageId']);
+			$this->page_url = $data['pageId'];
+		}
+		if ($eventKey == 'after_load_content') {
+			print("\n\t PATH: ".$data['filePath']);
+			print("\n\t BASE: ".\Phile\Utility::getBaseUrl());
+			print("\n\t ROOT: ".ROOT_DIR);
+			$result = str_replace(ROOT_DIR, \Phile\Utility::getBaseUrl()."/", $data['filePath']);
+			$result = str_replace(".md", "/", $result);
+			print("\n\t RESULT: ".$result);
+//			print("\n\n\n\t PageURL after: ".$this->page_url);
+//			print("\n\n\n\t RAW: ".$data['rawData']);
+//			print("\n\n\n\t PAGE: ".serialize($data['page']));
+		}
+		if ($eventKey == 'before_parse_content') {
+			print_r(get_defined_vars());
+		}
+		if ($eventKey == 'after_parse_content') {
+			print_r(get_defined_vars());
+		}
+		print("\n\n");
+	}
+
+	private function filter_content($content, $path) {
+		// return nothing if no content is available for processing
+		if (!isset($content)) return null;
+
+		// Images
+		$regex = "/(<p>)(.*?)\.(jpg|jpeg|png|gif|webp|svg)+(<\/p>)/i";
+		$replace = "\n".'<'.$this->settings['wrap_element'].' class="'.$this->settings['wrap_class_img'].'" style="background-image: url(\''.$path.'$2.$3\');"></'.$this->settings['wrap_element'].'>';
+		// replace image strings with image embeds
+		$content = preg_replace($regex, $replace, $content);
+
+		// Vimeo
+		$regex = "/(<p>)(http\S+vimeo\.com\/)(\d*)(<\/p>)/i";
+//		$replace = "\n".'<iframe class="'.$this->settings['wrap_class_vid'].'" src="//player.vimeo.com/video/$3?title=0&amp;byline=0&amp;portrait=0&amp;color=b2b0af" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+		$replace = "\n".'<iframe class="'.$this->settings['wrap_class_vid'].'" src="//player.vimeo.com/video/$3?title=0&amp;byline=0&amp;portrait=0&amp;color=b2b0af" width="100%" height="100%" allowfullscreen></iframe>';
+		// replace Vimeo links with Vimeo embeds
+		$content = preg_replace($regex, $replace, $content);
+
+		// YouTube - additional URL solutions from http://stackoverflow.com/questions/3392993/php-regex-to-get-youtube-video-id
+		$regex = "/(<p>)(http\S+youtube\.com\/watch\?v=)(\S+)(<\/p>)/i";
+//		$replace = "\n".'<iframe class="'.$this->settings['wrap_class_vid'].'" src="https://www.youtube.com/embed/$3" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+		$replace = "\n".'<iframe class="'.$this->settings['wrap_class_vid'].'" src="https://www.youtube.com/embed/$3" width="100%" height="100%" allowfullscreen></iframe>';
+		// replace YouTube links with Vimeo embeds
+		$content = preg_replace($regex, $replace, $content);
+
+		return $content;
+	}
+
+/*
 	private function filter_media($content, $path) {
 		// return nothing if no content is available for processing
 		if (!isset($content)) return null;
@@ -88,4 +164,6 @@ class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\Even
 
 		return $content;
 	}
+//*/
+
 }
